21d96c9da6e8a2bcbb2e34452dbe0d81caae42cf
gangeshsingh72 <singh.gangesh@gmail.com>
2020-06-29 18:53:32 +0100

    ImportsExample

---
+++ src/Imports.java
@@ -0,0 +1,10 @@
+import java.sql.Array;
+import java.lang.*;
+import java.util.Random;
+
+public class Imports {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(99));
+    }
+}
